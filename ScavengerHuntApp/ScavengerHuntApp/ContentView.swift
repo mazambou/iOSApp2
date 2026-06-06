f029573f23b82e8e15de0af55aa6e07e8b289dd1
AZAMBOU Marcel <azambou@mac.home>
2026-06-05 23:39:28 -0400

    Initial Commit

---
+++ ScavengerHuntApp/ScavengerHuntApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScavengerHuntApp
+//
+//  Created by AZAMBOU Marcel on 2026-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScavengerHuntApp/ScavengerHuntApp/ScavengerHuntAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntAppApp.swift
+//  ScavengerHuntApp
+//
+//  Created by AZAMBOU Marcel on 2026-06-05.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
